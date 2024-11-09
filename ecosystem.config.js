dc7f38081d69bc442acee324de7663b6834a82bf
itzmatt019 <itzmatt019@gmail.com>
2024-11-09 14:38:09 -0800

    wfefwef

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps : [{
     name: "itzmattportfolio",
-    script: "npm",
-    args: "start -p 4783",
+    script: "npm start -p 4783",
+    // args: "start -p 4783",
     cwd: "/home/itzmattportfolio/src",
     watch: true,
     env: {
