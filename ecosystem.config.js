84d6846d8b6e45dcbc6001a3b1e90a9f42c3f6a0
itzmatt019 <itzmatt019@gmail.com>
2024-11-09 14:18:52 -0800

    feat(update): Edited ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: "itzmattportfolio",
     script: "npm",
-    args: "start",
+    args: "start -p 4783",
     cwd: "/home/itzmattportfolio/src",
     watch: true,
     env: {
