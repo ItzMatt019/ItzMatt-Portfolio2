59bfa76d0c8c17fc1eccbaf1968bef9e95376d33
itzmatt019 <itzmatt019@gmail.com>
2024-11-09 16:27:39 -0800

    fefefefewf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: "itzmattportfolio",
     script: "node_modules/next/dist/bin/next",
-    args: "start -p 4783",
+    args: "start -p 8080",
     cwd: "/home/itzmattportfolio/src",
     env: {
       NODE_ENV: "production",
